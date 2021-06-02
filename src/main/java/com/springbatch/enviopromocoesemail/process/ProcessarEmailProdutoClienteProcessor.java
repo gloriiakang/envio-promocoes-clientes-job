784852bba6a529abeac25b04e106b59ac485c9c8
gloriikang <lori.vihchoi@gmail.com>
2021-06-02 18:41:00 -0300

    Creating writer

--- src/main/java/com/springbatch/enviopromocoesemail/process/ProcessarEmailProdutoClienteProcessor.java
+++ src/main/java/com/springbatch/enviopromocoesemail/process/ProcessarEmailProdutoClienteProcessor.java
@@ -3,21 +3,22 @@ package com.springbatch.enviopromocoesemail.process;
 import java.text.NumberFormat;
 
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
 import com.springbatch.enviopromocoesemail.domain.InteresseProdutoCliente;
 
-@Configuration
+@Component
 public class ProcessarEmailProdutoClienteProcessor implements ItemProcessor<InteresseProdutoCliente, SimpleMailMessage> {
 
 	@Override
-	public SimpleMailMessage process(InteresseProdutoCliente interesseProdutoCliente) {
+	public SimpleMailMessage process(InteresseProdutoCliente interesseProdutoCliente) throws Exception {
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setFrom("email@no-reply.com");
 		email.setTo(interesseProdutoCliente.getCliente().getEmail());
 		email.setSubject("Promoção!");
 		email.setText(gerarTextoPromocao(interesseProdutoCliente));
+		Thread.sleep(2000);
 		
 		return email;
 	}
